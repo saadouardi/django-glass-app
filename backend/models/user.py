ac939419b12e1f6ede93ecbbd82dbd8991777a62
SaadOuardi <saad.ouardi.developer@gmail.com>
2025-01-22 01:15:08 +0100

    add users

--- backend/models/user.py
+++ backend/models/user.py
@@ -2,5 +2,6 @@ from django.db import models # type: ignore
 
 class User(models.Model):
     id = models.AutoField(primary_key=True)
+    # uuid = models.UUIDField(unique=True)
     username = models.CharField(max_length=100, unique=True, null=False)
     password = models.CharField(min_length=8)
